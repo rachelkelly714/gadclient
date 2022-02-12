5d7c1b79d268d6844642022dab20232d111f72ee
Rachel Kelly <rachelkelly7755@gmail.com>
2022-02-12 16:56:30 -0500

    2/12/22 Finished Interfaces file

--- src/App.tsx
+++ src/App.tsx
@@ -5,81 +5,13 @@ import server from './helpers/DB';
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom"; 
 import Login from '../src/components/Login';
 import Register from '../src/components/Register';
-import Interface from '../src/components/Interfaces'
+import {Flytoken, Usercred, Philocred, Admincred, Serverfetch} from '../src/components/Interfaces'; 
  
-
-export interface Flytoken {
-
-  userLoggedIn: boolean; 
-  setUserLoggedIn: (value : boolean) => void; 
-  clearToken: () => void; 
-  sessionToken: string | null; 
-  setSessionToken: (token: string | null) => void; 
-  updateToken: (newToken: string) => void; 
-
-}
-
-export interface Usercred {
-
-  id: string; 
-  setId: (id: string) => void; 
-  emailAddress: string | null;
-  setEmailAddress: (email: string | null) => void; 
-  username: string;
-  setUsername: (username: string) => void; 
-  isAdmin: boolean | null; 
-  setIsAdmin: (isAdmin: boolean | null) => void; 
-  isPhilo: boolean | null; 
-  setIsPhilo: (isPhilo: boolean | null) => void; 
-}; 
-
-export interface Philocred {
-  id: string; 
-  emailPAddress: string | null; 
-  setEmailPAddress: (email: string | null) => void;
-  usernameP: string; 
-  setUserNameP: (usernameP: string) => void; 
-  isPhilo: boolean | null; 
-  setIsPhilo: ( isPhilo: boolean | null) => void; 
-  isAdmin: boolean | null;
-  setIsAdmin: (isAdmin: boolean | null ) => void; 
-}
-
-
-export interface Admincred {
-
-id: string; 
-emailAAdress: string | null; 
-setEmailAAdress: (emailAAdress: string | null) => void; 
-usernameA: string; 
-setUserNameA: (usernameA: string) => void; 
-isAdmin: boolean | null; 
-setIsAdmin: (isAdmin: boolean | null) => void;
-
-}
-
-export interface Serverfetch {
-  errorMessage: string; 
-  setErrorMessage: (errorMessage: string) => void; 
-  fetchServer: () =>Promise<void>; 
-  responseCall: number; 
-  setResponseCall: (responseCall: number) => void; 
-}
 
 
 
 const App = () => {
-  // ~~** User States **~~// 
-
-  const [id, setId] = useState<Usercred['id']>('');  
-  const [username, setUsername] = useState<Usercred['username']>(''); 
-  const [emailAddress, setEmailAddress] = useState<Usercred['emailAddress']>(''); 
-  const [isPhilo, setIsPhilo] = useState<Usercred['isPhilo']>('');
-  const [isAdmin, setIsAdmin] = useState<Usercred['isAdmin']>(''); 
-  const [sessionToken, setSessionToken] = useState<Flytoken['sessionToken']>(''); 
-  const [userLoggedIn, setUserLoggedIn] = useState<Flytoken['sessionToken']>(''); 
-  const [errorMessage, setErrorMessage] = useState<Serverfetch['errorMessage']>('');
-  const []
+ 
 
 
 }
--- src/components/Interfaces.tsx
+++ src/components/Interfaces.tsx
@@ -1,8 +1,66 @@
 import React from 'react'; 
 import server from '../helpers/DB'; 
-import {Usercred, Philocred, Admincred, Flytoken} from '../App'
 
 
+export interface Flytoken {
+
+    userLoggedIn: boolean; 
+    setUserLoggedIn: (value : boolean) => void; 
+    clearToken: () => void; 
+    sessionToken: string | null; 
+    setSessionToken: (token: string | null) => void; 
+    updateToken: (newToken: string) => void; 
+  
+  }
+  
+  export interface Usercred {
+  
+    id: string; 
+    setId: (id: string) => void; 
+    emailAddress: string | null;
+    setEmailAddress: (email: string | null) => void; 
+    username: string;
+    setUsername: (username: string) => void; 
+    isAdmin: boolean | null; 
+    setIsAdmin: (isAdmin: boolean | null) => void; 
+    isPhilo: boolean | null; 
+    setIsPhilo: (isPhilo: boolean | null) => void; 
+  }; 
+  
+  export interface Philocred {
+    id: string; 
+    emailPAddress: string | null; 
+    setEmailPAddress: (email: string | null) => void;
+    usernameP: string; 
+    setUserNameP: (usernameP: string) => void; 
+    isPhilo: boolean | null; 
+    setIsPhilo: ( isPhilo: boolean | null) => void; 
+    isAdmin: boolean | null;
+    setIsAdmin: (isAdmin: boolean | null ) => void; 
+  }
+  
+  
+  export interface Admincred {
+  
+  id: string; 
+  emailAAdress: string | null; 
+  setEmailAAdress: (emailAAdress: string | null) => void; 
+  usernameA: string; 
+  setUserNameA: (usernameA: string) => void; 
+  isAdmin: boolean | null; 
+  setIsAdmin: (isAdmin: boolean | null) => void;
+  
+  }
+  
+  export interface Serverfetch {
+    errorMessage: string; 
+    setErrorMessage: (errorMessage: string) => void; 
+    fetchServer: () =>Promise<void>; 
+    responseCall: number; 
+    setResponseCall: (responseCall: number) => void; 
+  }
+  
+  
 
 interface SetProps {
 
@@ -15,3 +73,5 @@ interface SetProps {
     
 
 }
+
+
